530fe2cea66b7fb1778cc976df7d0802f3a017fb
Alex Wong <alex.wong.u@gmail.com>
2020-04-14 21:04:51 -0400

    finished state function

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -6,12 +6,108 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "lexer.h"
 
+int move_state(int state, char curr){ 
+	if ((state == OPEN_BRACE) | (state == CLOSED_BRACE)) {
+		return -1;
+	} 
+	if ((state == OPEN_PAREN) | (state == CLOSED_PAREN)) {
+		return -1;
+	} 
+	if (state == SEMICOLON) {
+		return -1;
+	} 
+	if (state == 0) { // Empty State
+		switch(curr) {
+			case '{' :
+				return OPEN_BRACE;
+				break;
+			case '}' :
+				return CLOSED_BRACE;
+				break;
+			case '(' :
+				return OPEN_PAREN;
+				break;
+			case ')' :
+				return CLOSED_PAREN;
+				break;
+			case ';' :
+				return SEMICOLON;
+				break;
+			case ' ' :
+				return WHITE_SPACE;
+			case '\n' :
+				return WHITE_SPACE;
+			case '\t' :
+				return WHITE_SPACE;
+			}
+		if (isdigit(curr)) {
+			return INTEGER;
+		}
+		if (isalpha(curr)) {
+			return IDENTIFIER;
+		}
+	}
+	if (state == INTEGER) {
+		if (isdigit(curr)) {
+			return INTEGER;
+		} else { 
+			return INVALID;
+		}
+	}
+	if (state == IDENTIFIER) {
+		if (isalpha(curr)) {
+			return IDENTIFIER2;
+		} else {
+			return INVALID;
+		}
+	}
+	if (state == IDENTIFIER2) {
+		if ((isalnum(curr)) | (curr == '_')) {
+			return IDENTIFIER2;
+		} else {
+			return INVALID;
+		}
+	}
+
+	return -1;
+}
+
 int main(int argc, char **argv) {
+	FILE *source;
+	FILE *dest;
+	source = fopen(argv[1], "r");
+	if (!source) {
+		perror("Source failed to open");
+		exit(1);
+	}
+	dest = fopen("tokens", "w");
+	if (!dest) {
+		perror("Destination failed to open");
+		exit(1);
+	}
+        	
+	char curr;
+	int state = 0;
 
-	
+	while (fread(&curr, sizeof(char), 1, source) == sizeof(char)) {
+		state = move_state(state, curr);
+		if (state == INVALID) {
+			// Do Stuff
+			
+		}
 
-
+	}
+	/*
+	fread(&curr, sizeof(char), 1, source);
+	fread(&curr, sizeof(char), 1, source);
+	fseek(source, -sizeof(char), SEEK_CUR);
+	curr = 'a';
+	fread(&curr, sizeof(char), 1, source);
+	fwrite(&curr, sizeof(char), 1, dest);
+	*/
 	return 0;
 }
--- lexer/lexer.h
+++ lexer/lexer.h
@@ -1,3 +1,16 @@
 /* Lexer definitions and possible helpers */
 
+/** State numbers
+ */
+#define INVALID -1
+#define OPEN_BRACE 1
+#define CLOSED_BRACE 2
+#define OPEN_PAREN 3
+#define CLOSED_PAREN 4
+#define SEMICOLON 5
+#define INTEGER 6
+#define IDENTIFIER 7
+#define IDENTIFIER2 8
+#define WHITE_SPACE 9
+
 
